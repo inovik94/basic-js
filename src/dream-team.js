b880808596740968a91f3bc5c525ce1948b9333b
“inovik94” <ilya.novikk@gmail.com>
2021-03-29 17:21:05 +0300

    add: add functionality for dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
+// let dreamteam = ['Yana','Alena','Malena',true,123]
+// createDreamTeam(dreamteam);
+// console.log(members);
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(dreamteam) {
+  if (!Array.isArray(dreamteam)) return false;
+  return dreamteam
+    .filter((el) => typeof el == "string")
+    .map((e) => e
+      .toLowerCase()
+      .replace(/\s/g, "")
+      .charAt(0)
+      .toUpperCase())
+    .sort()
+    .join("");
 };
